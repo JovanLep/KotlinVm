92889d823418e6986034479d1845a0af27fc25c7
lierpeng <lierpeng@zhen.com>
2020-07-22 16:08:05 +0800

    支付调整  arouter，xn

--- app/src/main/java/com/zhenpin/luxurystore/WelcomeActivity.kt
+++ app/src/main/java/com/zhenpin/luxurystore/WelcomeActivity.kt
@@ -1,16 +1,18 @@
 package com.zhenpin.luxurystore
 
 import android.os.Bundle
-//import com.alibaba.android.arouter.launcher.ARouter
+import android.view.View
+import com.alibaba.android.arouter.launcher.ARouter
 import com.zhen.mvvm.base.BaseActivity
 import com.zhen.mvvm.base.NoViewModel
 import com.zhenpin.luxurystore.databinding.ActivityWelcomeBinding
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : BaseActivity<NoViewModel, ActivityWelcomeBinding>() {
+class WelcomeActivity : BaseActivity<NoViewModel, ActivityWelcomeBinding>(), (View) -> Unit {
     override fun layoutId(): Int = R.layout.activity_welcome
 
     override fun initView(savedInstanceState: Bundle?) {
-
+        tv_add.setOnClickListener(View.OnClickListener(this))
     }
 
     override fun initData() {
@@ -18,7 +20,11 @@ class WelcomeActivity : BaseActivity<NoViewModel, ActivityWelcomeBinding>() {
     }
 
     fun startGuide() {
-//        ARouter.getInstance().build("/app/guide").navigation()
+        ARouter.getInstance().build("/app/guide").navigation()
+    }
+
+    override fun invoke(p1: View) {
+        startGuide()
     }
 
 }
